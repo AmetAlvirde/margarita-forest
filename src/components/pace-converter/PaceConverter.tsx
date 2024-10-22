1a1520f20819e732b94a6d453a40bd435a2598ca
Amet Alvirde <Amet.Alvirde@gmail.com>
2024-10-22 14:01:23 -0600

    Pace Converter General UI idea mock

--- src/components/pace-converter/PaceConverter.tsx
+++ src/components/pace-converter/PaceConverter.tsx
@@ -1,3 +1,72 @@
-export const PaceConverter = () => {
-  return <h1>hola</h1>;
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Separator } from "../ui/separator";
+
+type PaceConverterType = {
+  from?: string;
+  to?: string;
+  view?: string;
+  withPredictions?: boolean;
+};
+export const PaceConverter = ({
+  from = "minkm",
+  withPredictions = true,
+}: PaceConverterType) => {
+  return (
+    <>
+      <section className="flex">
+        <Input type="string" placeholder="05:30"></Input>
+        <Select>
+          <SelectTrigger className="w-40">
+            <SelectValue placeholder={from} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="minkm">min/km</SelectItem>
+            <SelectItem value="minmi">min/mi</SelectItem>
+            <SelectItem value="kph">Kph</SelectItem>
+            <SelectItem value="miph">Miph</SelectItem>
+          </SelectContent>
+        </Select>
+      </section>
+      <Separator
+        className="bg-gray-300 my-4"
+        orientation="horizontal"
+      ></Separator>
+      <section>
+        <ul>
+          <li>
+            <span className="text-2xl px-2 font-semibold">08:51</span>
+            <span className="italic">min/mi</span>
+          </li>
+        </ul>
+      </section>
+      {withPredictions ? (
+        <section>
+          <h2 className="text-3xl">Race predictions</h2>
+          <ul>
+            <li>
+              <span className="text-2xl px-2 font-semibold">08:51</span>
+              <span className="italic">min/mi</span>
+            </li>
+            <li>
+              <span className="text-2xl px-2 font-semibold">08:51</span>
+              <span className="italic">min/mi</span>
+            </li>
+            <li>
+              <span className="text-2xl px-2 font-semibold">08:51</span>
+              <span className="italic">min/mi</span>
+            </li>
+          </ul>
+        </section>
+      ) : (
+        <a>See race predictions with this pace</a>
+      )}
+    </>
+  );
 };
